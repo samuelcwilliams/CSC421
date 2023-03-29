40f80325c7ee3bbbe6a9bf0e84fdef1076f11b96
samuelcwilliams <samcwilliams67@gmail.com>
2023-03-28 19:49:06 -0500

    fixed static semantic error in problem4

--- lab1/problem4.c
+++ lab1/problem4.c
@@ -32,15 +32,41 @@ int main() {
 	fgets(num6, 25, stdin);
 
 	array2[0] = atoi(num4);
-	array2[0] = atoi(num5);
-	array2[0] = atoi(num6);
+	array2[1] = atoi(num5);
+	array2[2] = atoi(num6);
 
 	array3[0] = array1[0];
 	array3[1] = array1[1];
 	array3[2] = array1[2];
-	array3[3] = array1[0];
-	array3[4] = array1[1];
-	array3[5] = array1[2];
+	array3[3] = array2[0];
+	array3[4] = array2[1];
+	array3[5] = array2[2];
 
 	printf("\nThe merged array is [%lf, %lf, %lf, %lf, %lf, %lf]", array3[0], array3[1], array3[2], array3[3], array3[4], array3[5]);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
