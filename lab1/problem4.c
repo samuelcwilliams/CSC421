c2aa2f8f6b7a8694c8b426718f817b4c595ecaa5
samuelcwilliams <samcwilliams67@gmail.com>
2023-03-28 19:33:12 -0500

    problem 4 comp.

---
+++ lab1/problem4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char num1[25], num2[25], num3[25], num4[25], num5[25], num6[25];
+	double array1[3], array2[3], array3[6];
+	
+	printf("--------------------------------\n");
+	printf("Enter the first array's values: \n");
+	printf("--------------------------------\n");
+
+	printf("Enter a number: ");
+	fgets(num1, 25, stdin);
+	printf("Enter a number: ");
+	fgets(num2, 25, stdin);
+	printf("Enter a number: ");
+	fgets(num3, 25, stdin);
+
+	array1[0] = atoi(num1);
+	array1[1] = atoi(num2);
+	array1[2] = atoi(num3);
+
+	printf("--------------------------------\n");
+	printf("Enter the second array's values \n");
+	printf("--------------------------------\n");
+
+	printf("Enter a number: ");
+	fgets(num4, 25, stdin);
+	printf("Enter a number: ");
+	fgets(num5, 25, stdin);
+	printf("Enter a number: ");
+	fgets(num6, 25, stdin);
+
+	array2[0] = atoi(num4);
+	array2[0] = atoi(num5);
+	array2[0] = atoi(num6);
+
+	array3[0] = array1[0];
+	array3[1] = array1[1];
+	array3[2] = array1[2];
+	array3[3] = array1[0];
+	array3[4] = array1[1];
+	array3[5] = array1[2];
+
+	printf("\nThe merged array is [%lf, %lf, %lf, %lf, %lf, %lf]", array3[0], array3[1], array3[2], array3[3], array3[4], array3[5]);
+}
